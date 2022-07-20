dc55e4956d9ec243009398e7fe56b61d46dcc5b3
lucas <lucasgruden0@protonmail.com>
2022-07-20 00:14:24 -0300

    add files

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+def do_twice(func, arg):
+    func(arg)
+    func(arg)
+def do_four(func, arg):
+    do_twice(func, arg)
+    do_twice(func, arg)
+def right_justify(str):
+    print((70 - len(str)) * ' ' + str)
+
 if __name__ == '__main__':
-    print('Hello World')
+    do_twice(print, 'spam')
+    print('-' * 20)
+    do_four(print, 'spam')
+    print('-' * 20)
+    right_justify('96000000')
+
+
 
 
 '''
