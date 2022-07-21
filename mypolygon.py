c83e008bc051b4b1f0ef51732e2bac90184431d6
lucas <lucasgruden0@protonmail.com>
2022-07-21 15:03:55 -0300

    add files

--- mypolygon.py
+++ mypolygon.py
@@ -7,8 +7,18 @@ def polygon(t, n, length):
     for i in range(n):
         t.fd(length)
         t.lt(360/n)
-
-
+def circle(t, r):
+    circumference = 2 * 3.14 * r
+    n = 50
+    length = circumference / n
+    polygon(t, n, length)
+def arc(t, r, angle):
+    arc_length = 2 * 3.14 * r * angle / 360
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle = angle / n
+    for i in range(n):
+        t.fd(step_length)
+        t.lt(step_angle)
 bob = turtle.Turtle()
-polygon(bob, 6, 100)
 turtle.mainloop()
